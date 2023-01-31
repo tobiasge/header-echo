3e65069347bcca19fe903ad7381af22aa3790862
Tobias Genannt <tobias.genannt@gmail.com>
2023-01-31 15:00:59 +0100

    Echo more data

--- asgi.py
+++ asgi.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-@app.get("/")
-async def index(request: Request):
-    return request.headers
+
+@app.get("/{full_path:path}")
+@app.put("/{full_path:path}")
+@app.post("/{full_path:path}")
+@app.delete("/{full_path:path}")
+async def index(request: Request, full_path: str):
+    return {
+        "full_path": f"/{full_path}",
+        "method": request.method,
+        "params": request.query_params,
+        "headers": request.headers,
+        "body": await request.body(),
+    }
